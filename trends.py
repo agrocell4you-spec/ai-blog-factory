7c1bd3f27dfb0757f3e32b607784c09689ec5d08
Agrocell <agrocell4you@gmail.com>
2026-03-10 06:37:35 +0900

    Change trending keywords region from US to South Korea

--- trends.py
+++ trends.py
@@ -3,6 +3,7 @@ from pytrends.request import TrendReq
 def get_trending_keywords():
     pytrends = TrendReq(hl='en-US', tz=360)
 
-    df = pytrends.trending_searches(pn='united_states')
+    df = pytrends.trending_searches(pn='south_korea')
 
     return df[0].tolist()
+
